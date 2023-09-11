d6e9cdba62b8fb02952c94335587bcfcf61e757f
devsula50 <devsula50@gmail.com>
2023-09-12 03:23:12 +0900

    Ouputting conditional Content and Adding conditional return statements
    조건문을 통해서 return 하는 JSX 부분을 조작할 수 있는데 이때 if나
    for문을 사용하면 자연스럽게 {}를 사용하게 된다. 하지만 JSX안에서는 {}을
    여러차례 사용하는 것이 제한된다.
    따라서 첫 번째로 ternary operator를 사용한다. 하지만 이것은 JSX
    코드내부에 로직이 들어가며 코드를 읽기가 쉽지 않게 된다.
    그래서 두 번째로 로직을 바깥으로 빼서 정의하고 그 결과를 return 문에 반영하는
    방식도 있다.
    
    또한 바깥에서 로직을 분기했다면 그상태로 return 해버리는 것도
    가능하다.

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../ui/Card";
 import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
@@ -12,25 +13,17 @@ const Expenses = (props) => {
     setSelectedYear(year);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear() === parseInt(selectedYear);
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={selectedYear}
         onChangeYear={changeYearHandler}
       />
-      {props.items
-        .filter((expense) => {
-          // console.log(typeof expense.date.getFullYear(), typeof selectedYear);
-          return expense.date.getFullYear() === parseInt(selectedYear);
-        })
-        .map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
