38b011dc1e1e48df6bdb4c09fa0a8c86e604a0fe
devsula50 <devsula50@gmail.com>
2023-10-23 21:24:00 +0900

    Updated React Essentials section03

---
+++ section03/src/App.jsx
@@ -0,0 +1,73 @@
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import { Header } from "./components/Header/Header";
+import { CoreConcept } from "./components/CoreConcept/CoreConcept";
+import { TabButton } from "./components/TabButton";
+import { useState } from "react";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic</p>;
+
+  if (selectedTopic)
+    tabContent = (
+      <div id={"tab-content"}>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  return (
+    <>
+      <Header />
+      <main>
+        <section id={"core-concepts"}>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
+          </ul>
+        </section>
+        <section id={"examples"}>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              onSelect={() => handleSelect("components")}
+              isSelected={selectedTopic === "components"}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onSelect={() => handleSelect("jsx")}
+              isSelected={selectedTopic === "jsx"}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onSelect={() => handleSelect("props")}
+              isSelected={selectedTopic === "props"}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onSelect={() => handleSelect("state")}
+              isSelected={selectedTopic === "state"}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
