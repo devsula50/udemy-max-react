9bec6aecfaaebb67e54f5be0bca746bda488d785
devsula50 <devsula50@gmail.com>
2023-10-26 22:38:23 +0900

    styling 완료

--- section06/src/components/AuthInputs.jsx
+++ section06/src/components/AuthInputs.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Input from "./ui/Input.jsx";
+import { Button } from "./ui/Button.jsx";
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,36 +19,37 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+    <div
+      id="auth-inputs"
+      className={
+        "mx-auto w-full max-w-sm rounded bg-gradient-to-b from-stone-700 to-stone-800 p-8 shadow-md"
+      }
+    >
+      <div className={"mb-6 flex flex-col gap-2"}>
+        <Input
+          label={"Email"}
+          invalid={emailNotValid}
+          type="email"
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <Input
+          label={"Password"}
+          invalid={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="hover:text-amber-5 text-amber-400">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- section06/src/components/Header.jsx
+++ section06/src/components/Header.jsx
@@ -1,11 +1,21 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className={"mb-8 mt-8 flex flex-col items-center md:mb-16"}>
+      <img
+        src={logo}
+        alt="A canvas"
+        className={"mb-8 h-44 w-44 object-contain"}
+      />
+      <h1
+        className={
+          "font-title text-center text-xl font-semibold uppercase tracking-widest text-amber-800 md:text-4xl"
+        }
+      >
+        React Art
+      </h1>
+      <p className={"text-stone-500"}>A community of artists and art-lovers.</p>
     </header>
   );
 }
